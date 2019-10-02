67f1f0b8e0f1a66a7cb42c73293a6184a0a6cb96
LoganSQL <loganchen@hotmail.com>
2019-10-02 11:15:07 -0400

    connection string test

--- TestSQLAG1/Form1.Designer.vb
+++ TestSQLAG1/Form1.Designer.vb
@@ -22,9 +22,53 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(29, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(218, 21)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "SQLClient: ClickToConnetAGListener"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(29, 86)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(218, 21)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "SNAC ODBC: ClickToConnetAGListener"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(29, 146)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(218, 21)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "SNAC OLE DB: ClickToConnetAGListener"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "LoganSQLTestSQLAG"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- TestSQLAG1/Form1.vb
+++ TestSQLAG1/Form1.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Use SQLClient and ApplicationIntent
+        Dim SQLClientConn As New SqlClient.SqlConnection("data source=TestAG2Listener;integrated security=sspi;initial catalog=TSQL2012;applicationintent=readonly")
+        SQLClientConn.Open()
+        Dim objCmd As SqlClient.SqlCommand = New SqlClient.SqlCommand("select @@servername", SQLClientConn)
+        MsgBox(objCmd.ExecuteScalar())
+        SQLClientConn.Close()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Use SNAC ODBC And ApplicationIntent
+        Dim SNACODBCConn As New Odbc.OdbcConnection("Driver={SQL Server Native Client 11.0};server=TestAG2Listener;database=TSQL2012;trusted_connection=yes;applicationintent=readonly")
+        SNACODBCConn.Open()
+        Dim objCmd As Odbc.OdbcCommand = New Odbc.OdbcCommand("select @@servername", SNACODBCConn)
+        MsgBox(objCmd.ExecuteScalar())
+        SNACODBCConn.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Use SNAC SQL OLE DB And ApplicationIntent
+        Dim SNACOLEDBConn As New OleDb.OleDbConnection
+        SNACOLEDBConn.ConnectionString = "Provider=sqlncli11;data source=TestAG2Listener;integrated security=sspi;initial catalog=TSQL2012;application intent=readonly"
+        SNACOLEDBConn.Open()
+        Dim objCmd As OleDb.OleDbCommand = New OleDb.OleDbCommand("select @@servername", SNACOLEDBConn)
+        MsgBox(objCmd.ExecuteScalar())
+        SNACOLEDBConn.Close()
+    End Sub
 End Class
